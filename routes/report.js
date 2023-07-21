603aad0d6ff6990cf838de3a537c02dedee5346f
asahitoyotae <asahitoyotae@gmail.com>
2023-07-21 16:36:24 +0800

    send to email

--- routes/report.js
+++ routes/report.js
@@ -1,8 +1,43 @@
 const router = require("express").Router();
 const message = require("./schema");
+const nodemailer = require("nodemailer");
 
 router.post("/report", async (req, res) => {
   const date = new Date(Date.now());
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
+    auth: {
+      user: "asahitoyotae@gmail.com",
+      pass: process.env.EMAIL_SECRET,
+    },
+  });
+
+  const mailOptions = {
+    from: "asahitoyotae@gmail.com",
+    to: "asahitoyotae@gmail.com",
+    subject: "Canding_REPORTS/FEEDBACK",
+    html: `<html>
+        <body>
+          <div style="padding: 3rem; width: 50%; margin: 50px auto; border: 1px solid black; border-radius: 15px;">
+            <h1 style="text-align: center">Canding - chatBot</h1>
+            <p style="text-align: center; color: gray; font-size: 1rem; font-weight: bold">
+             user email: ${req.body.email}
+            </p>
+            <p style="text-align: center; color: gray; font-size: 1rem; font-weight: bold">
+             user feedback: ${req.body.message}
+            </p>
+            <p style="color: gray; text-align: center;">
+              I am receving this message because someone reported and issue or giving a feedback
+            </p>
+          </div>
+        </body>
+      </html>`,
+  };
+
   try {
     const report = new message({
       email: req.body.email,
@@ -14,7 +49,16 @@ router.post("/report", async (req, res) => {
 
     const saveReport = await report.save();
     if (saveReport) {
-      res.status(200).send({ success: true, message: "report sent" });
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          res
+            .status(500)
+            .send({ sucess: false, message: "failed to send report" });
+        } else {
+          console.log(info);
+          res.status(200).send({ success: true, message: "report sent" });
+        }
+      });
     } else {
       res.status(500).send({ sucess: false, message: "failed to send report" });
     }
